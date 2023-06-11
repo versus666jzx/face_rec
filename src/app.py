f9c8f2e048b49a5a0f9e1ea5a14e3b5eac8fcf08
HardWorkingStation <hws@my.power>
2023-06-11 03:14:37 +0300

    Initial commit

--- src/app.py
+++ src/app.py
@@ -9,7 +9,7 @@ from pathlib import Path
 from typing import List, NamedTuple
 
 import av
-import cv2
+# import cv2
 import numpy as np
 import streamlit as st
 from streamlit_webrtc import WebRtcMode, webrtc_streamer
